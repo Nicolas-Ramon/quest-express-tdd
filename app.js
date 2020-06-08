263923dc2a542a68869045d479d8404bbc43ec93
Nico-toulouse <nicoramon@hotmail.com>
2020-06-08 10:18:19 +0200

    1st test passed

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Crée une route répondant à un GET sur '/' //
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: 'Hello World!'
+    })
+});
 
 module.exports = app;
